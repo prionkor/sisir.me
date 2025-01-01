d34b983e53e423330853094afb4dc2dafbd4d0d7
Sisir Kanti Adhikari <sisir@Sisirs-MacBook-Pro.local>
2025-01-01 16:43:01 +0600

    add cloudflare support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,23 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://sisir.me',
-	trailingSlash: 'never',
-	integrations: [
-		tailwind({
-			nesting: true,
-		}),
+  site: 'https://sisir.me',
+  trailingSlash: 'never',
+
+  integrations: [
+      tailwind({
+          nesting: true,
+      }),
 	],
-	build: {
-		format: 'directory',
-		assets: 'assets',
+
+  build: {
+      format: 'directory',
+      assets: 'assets',
 	},
+
+  adapter: cloudflare(),
 });
