0652877237bba0ab6a0e188ad82309c4ca9b5250
Sisir Adhikari <sisir@codeware.io>
2026-04-04 16:23:12 +0600

    site update & sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,21 +2,21 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://sisir.me',
-	trailingSlash: 'never',
+    site: 'https://sisir.me',
+    trailingSlash: 'never',
 
-	integrations: [
-		tailwind({
-			nesting: true,
-		}),
-	],
+    integrations: [tailwind({
+        nesting: true,
+		}), sitemap()],
 
-	build: {
-		format: 'directory',
-		assets: 'assets',
-	},
+    build: {
+        format: 'directory',
+        assets: 'assets',
+    },
 
-	//   adapter: cloudflare(), // needed when server-side logic is present, kept for future reference
+    //   adapter: cloudflare(), // needed when server-side logic is present, kept for future reference
 });
