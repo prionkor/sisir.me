23f503ac375ae040e109cf77364c790c8228ff28
Sisir Adhikari <sisir@codeware.io>
2026-04-05 20:11:55 +0600

    Add gtag

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,19 +4,29 @@ import tailwind from '@astrojs/tailwind';
 
 import sitemap from '@astrojs/sitemap';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://sisir.me',
-    trailingSlash: 'never',
+	site: 'https://sisir.me',
+	trailingSlash: 'never',
 
-    integrations: [tailwind({
-        nesting: true,
-		}), sitemap()],
+	integrations: [
+		tailwind({
+			nesting: true,
+		}),
+		sitemap(),
+		partytown({
+			config: {
+				forward: ['dataLayer.push'],
+			},
+		}),
+	],
 
-    build: {
-        format: 'directory',
-        assets: 'assets',
-    },
+	build: {
+		format: 'directory',
+		assets: 'assets',
+	},
 
-    //   adapter: cloudflare(), // needed when server-side logic is present, kept for future reference
+	//   adapter: cloudflare(), // needed when server-side logic is present, kept for future reference
 });
